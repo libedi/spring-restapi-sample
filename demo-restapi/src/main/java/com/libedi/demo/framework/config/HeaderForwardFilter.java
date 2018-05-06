97bea715a99d818c6f792e19e862ba9464d93d7c
Sangjun, Park <libedi@gmail.com>
2018-05-07 00:54:53 +0900

    class name refactoring

--- demo-restapi/src/main/java/com/libedi/demo/framework/config/HeaderForwardFilter.java
+++ demo-restapi/src/main/java/com/libedi/demo/framework/config/HeaderForwardFilter.java
@@ -11,16 +11,22 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+/**
+ * HeaderForwardFilter
+ * - 특정 요청 헤더값을 응답 헤더값으로 포워딩
+ * @author Sangjun, Park
+ *
+ */
 @Component
-public class HeaderSendingFilter extends OncePerRequestFilter {
+public class HeaderForwardFilter extends OncePerRequestFilter {
 	
 	private final String HEADER = "X-HEADER";
 
 	@Override
-	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-			throws ServletException, IOException {
+	protected void doFilterInternal(final HttpServletRequest request, final HttpServletResponse response,
+			final FilterChain filterChain) throws ServletException, IOException {
 		final String header = request.getHeader(HEADER);
-		if(StringUtils.hasLength(header)) {
+		if (StringUtils.hasLength(header)) {
 			response.addHeader(HEADER, header);
 		}
 		filterChain.doFilter(request, response);
