2fc4f1271a43f37ec7354833cb9af87141767f9f
Sangjun, Park <Sangjun, Park@TD3P127593.SKCC.NET>
2018-05-14 14:15:39 +0900

    modify CustomerControllerTest class

--- demo-restapi/src/test/java/com/libedi/demo/customer/control/CustomerControllerTest.java
+++ demo-restapi/src/test/java/com/libedi/demo/customer/control/CustomerControllerTest.java
@@ -1,9 +1,10 @@
 package com.libedi.demo.customer.control;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Before;
@@ -14,29 +15,39 @@ import org.junit.runners.MethodSorters;
 import com.libedi.demo.customer.model.Customer;
 import com.libedi.demo.customer.service.CustomerService;
 
+/**
+ * CustomerControllerTest
+ * @author Sangjun, Park
+ *
+ */
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class CustomerControllerTest {
 	
 	private CustomerController customerController;
 	
+	private CustomerService customerService;
+	
 	@Before
 	public void init() throws Exception {
-		this.customerController = new CustomerController(new CustomerService(null));
+		this.customerService = mock(CustomerService.class);
+		this.customerController = new CustomerController(customerService);
 	}
 	
 	@Test
 	public void test01_getCustomer() throws Exception {
 		Integer customerId = 1;
-		Customer customer = this.customerController.getCustomer(customerId);
-		assertNotNull(customer);
-		assertEquals(customer.getCustomerId(), customerId);
+		Customer expected = new Customer(customerId, "libedi", "Freelancer");
+		when(this.customerService.getCustomer(customerId)).thenReturn(expected);
+		Customer actual = this.customerController.getCustomer(customerId);
+		assertThat(actual).isNotNull().isEqualTo(expected);
 	}
 	
 	@Test
 	public void test02_getCustomerList() throws Exception {
-		List<Customer> customerList = this.customerController.getCustomerList();
-		assertNotNull(customerList);
-		assertFalse(customerList.isEmpty());
+		List<Customer> expected = Arrays.asList(new Customer(1, "libedi", "Freelancer"));
+		when(this.customerService.getCustomerList()).thenReturn(expected);
+		List<Customer> actual = this.customerController.getCustomerList();
+		assertThat(actual).isNotEmpty().isEqualTo(expected);
 	}
 
 	@Test
@@ -50,10 +61,10 @@ public class CustomerControllerTest {
 	@Test
 	public void test04_updateCustomer() throws Exception {
 		Integer customerId = 1;
-		Customer customer = new Customer();
-		customer.setCustomerName("libedi01");
-		Customer updateCustomer = this.customerController.updateCustomer(customerId, customer);
-		assertEquals(customer.getCustomerName(), updateCustomer.getCustomerName());
+		Customer expected = new Customer(customerId, "Sangjun,Park", "Kakao");
+		when(this.customerService.getCustomer(customerId)).thenReturn(expected);
+		Customer actual = this.customerController.updateCustomer(customerId, expected);
+		assertThat(actual).isNotNull().isEqualTo(expected);
 	}
 	
 	@Test
